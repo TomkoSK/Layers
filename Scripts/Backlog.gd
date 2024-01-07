8b67221c3d0123f3a77beebca1fe6852478096e5
TomkoSK <tomaslukac599@gmail.com>
2024-01-07 17:43:16 +0100

    Fixed backlog issues, backlog is now complete except for scrolling

--- Scripts/Backlog.gd
+++ Scripts/Backlog.gd
@@ -7,9 +7,6 @@ var testNode
 #Array of text and names for each line, the backlog has 12 lines so the array has 12 elements 
 var names = ["", "", "", "", "", "", "", "", "", "", "", ""]
 var textLines = ["", "", "", "", "", "", "", "", "", "", "", ""]
-#Just used when resetting the names and textLines arrays back to their empty state in updateArrays()
-var namesReset = ["", "", "", "", "", "", "", "", "", "", "", ""]
-var textLinesReset = ["", "", "", "", "", "", "", "", "", "", "", ""]
 
 func _ready():
 	self.hide()
@@ -25,24 +22,39 @@ func refreshText():#Just gets the text and names from names and textLines and pu
 		$CharacterNames.text += names[i]+"\n"
 
 func updateArrays():#Updates the text in names and textLines based on whatever is in the formattedText
+	var previousName = ""
 	var textArray#NOTE: put comments here explaining whats happening
-	textLines = textLinesReset
-	names = namesReset
+	for i in range(12):
+		names[i] = ""
+		textLines[i] = ""
 	if(len(formattedText) > 12):
-		textArray = formattedText.slice(-12, -1)
+		textArray = formattedText.slice(-12)
 	else:
 		textArray = formattedText
 	for i in range(len(textArray)):
 		if(len(textArray[i]) > 1):
-			names[i] = textArray[i][0]
 			textLines[i] = textArray[i][1]
+			if(previousName == textArray[i][0]):
+				pass
+			else:
+				previousName = textArray[i][0]
+				names[i] = textArray[i][0] 
 		else:
 			textLines[i] = textArray[i][0]
 	refreshText()
 
+func removeNewlines(text):
+	while(text.find("\n") != -1):#Keeps looping until it can't find any more \n characters
+		text = text.substr(0, text.find("\n"))+" "+text.substr(text.find("\n")+1)#Makes the string a substring of itself with every character except the \n
+		#The space is added where the \n used to be 
+		#because without the newline, there needs to be something between the words (there is no space between the words)
+		#idk how to explain it well just please dont remove the space :3
+	return text
+
 func addText(text):#textlines with the character name and the text will be passed to this function, in a format of ["CharacterName", "text"]
 	var charName = text[0]
 	#All of this is stolen from the textbox script and changed a bit :3, it breaks down the text into individual lines
+	text[1] = removeNewlines(text[1])
 	var textArray = text[1].split(" ")#Splits the text into individual words
 	var textLineArray = []#Array of the text split into individual lines
 	var index = 0
@@ -62,5 +74,11 @@ func addText(text):#textlines with the character name and the text will be passe
 	formattedText.append([charName, textLineArray[0]])
 	if(len(textLineArray) > 1):
 		for i in range(1, len(textLineArray)):
-			formattedText.append(textLineArray[i])
+			formattedText.append([textLineArray[i]])
 	updateArrays()
+
+func _input(event):
+	if(event.is_action("ui_up")):
+		self.show()
+	if(event.is_action("ui_down")):
+		self.hide()
