ba769689f053cf24bc1108065ef0e08fa8a3ead7
TomkoSK <tomaslukac599@gmail.com>
2024-01-29 17:33:37 +0100

    reworked SceneManager.gd to use tweens (god bless tweens)

--- Autoloaded/SceneManager.gd
+++ Autoloaded/SceneManager.gd
@@ -3,12 +3,6 @@ extends Node
 signal sceneSwitched
 
 var developing = false#MAKES ALL THE TRANSITIONS INSTANT TURN OFF WHEN UPLOADING NEW VERSION TO BE PLAYED
-var sprite : Sprite2D
-var opacity : float = 0
-var fadingAway : bool
-var fadingIn : bool
-var fadeLengthIn : float
-var fadeLengthOut : float
 var bgSprite : Sprite2D
 
 func _ready():
@@ -38,38 +32,26 @@ func changeScene(targetScene, fadeIn = 0.4, midLength = 0.4, fadeOut = 0.4):
 		fadeOut = 0.01
 	$CanvasLayer/Button.show()
 	var scene = load(targetScene).instantiate()
-	fadeLengthIn = fadeIn
-	fadeLengthOut = fadeOut
-	sprite = Sprite2D.new()
+	var sprite = Sprite2D.new()
 	sprite.texture = load("res://Textures/square.png")
 	sprite.scale = Vector2(4000, 4000)
 	sprite.position = Vector2(0, 0)
 	sprite.modulate = Color(0, 0, 0, 0)
 	sprite.z_index = 20
 	self.add_child(sprite)
-	fadingIn = true
-	await get_tree().create_timer(fadeLengthIn).timeout
-	fadingIn = false
-	opacity = 1
+	var tween = self.create_tween()
+	tween.tween_property(sprite, "modulate", Color(0, 0, 0, 1), fadeIn)
+	await get_tree().create_timer(fadeIn).timeout
 	get_tree().current_scene.queue_free()
 	get_tree().root.add_child(scene)
 	get_tree().set_current_scene(scene)
 	sceneSwitched.emit()
 	await get_tree().create_timer(midLength).timeout
-	fadingAway = true
-	await get_tree().create_timer(fadeLengthOut).timeout
-	fadingAway = false
-	opacity = 0
+	tween = self.create_tween()
+	tween.tween_property(sprite, "modulate", Color(0, 0, 0, 0), fadeOut)
+	await get_tree().create_timer(fadeOut).timeout
 	sprite.queue_free()
 	$CanvasLayer/Button.hide()
 
 func setBackground(bgPath):
 	bgSprite.texture = load(bgPath)
-
-func _process(delta):
-	if(fadingAway):
-		opacity -= 1*(delta/fadeLengthOut)
-		sprite.modulate.a = opacity
-	if(fadingIn):
-		opacity += 1*(delta/fadeLengthIn)
-		sprite.modulate.a = opacity
