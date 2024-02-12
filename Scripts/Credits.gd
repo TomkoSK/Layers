f4a7c3a29e6e6068ab2d77bc6a70eb17de41e06c
TomkoSK <tomaslukac599@gmail.com>
2024-02-12 10:36:32 +0100

    Added scrolling with arrow keys

--- Scripts/Credits.gd
+++ Scripts/Credits.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var velocity = 0
+var maxSpeed = 500
+
 func _ready():
 	UIButtons.set_visibility("ui", false)
 	$Camera2D/BackButton.modulate.a = 0.76
@@ -24,3 +27,24 @@ func _input(event):
 				$Camera2D.position.y = clamp($Camera2D.position.y, 540, 750)
 	if(event.is_action_pressed("ui_cancel")):
 		$Camera2D/BackButton.emit_signal("pressed")
+
+func _process(delta):#unnecesarily complicated smooth scrolling when using the arrows
+	if(Input.is_action_pressed("ui_up")):
+		velocity -= 1200*delta
+		velocity = clamp(velocity, -maxSpeed, maxSpeed)
+	elif(Input.is_action_pressed("ui_down")):
+		velocity += 1200*delta
+		velocity = clamp(velocity, -maxSpeed, maxSpeed)
+	else:
+		if(velocity < 0):
+			if(velocity >= -42):
+				velocity = 0
+			else:
+				velocity += 42
+		elif(velocity > 0):
+			if(velocity <= 42):
+				velocity = 0
+			else:
+				velocity -= 42
+	$Camera2D.position.y += velocity*delta
+	$Camera2D.position.y = clamp($Camera2D.position.y, 540, 750)
--- Scripts/Gallery.gd
+++ Scripts/Gallery.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var frameTexture = preload("res://Gallery/CGframe.png")
 var images = []
+var velocity = 0
+var maxSpeed = 650
 
 func _ready():
 	UIButtons.set_visibility("ui", false)
@@ -9,18 +11,16 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.is_pressed():
+		if event.is_pressed(): 
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 				$Camera2D.position.y -= 20
-				$Camera2D.position.y = clamp($Camera2D.position.y, 0, max(len(images)*270-900, 0))
 			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 				$Camera2D.position.y += 20
-				$Camera2D.position.y = clamp($Camera2D.position.y, 0, max(len(images)*270-900, 0))
 	if(event.is_action_pressed("ui_accept")):
 		addImage("res://Gallery/testBG.png")
 	if(event.is_action_pressed("ui_cancel")):
-		$MenuButton.emit_signal("pressed")
-		
+		$BackButton.emit_signal("pressed")
+
 func addImage(imagePath : String):
 	var offset = len(images)
 	var x
@@ -70,3 +70,28 @@ func _on_back_button_mouse_exited():
 
 func _on_back_button_mouse_entered():
 	$BackButton.modulate.a = 0.9
+
+func _process(delta):#unnecesarily complicated smooth scrolling when using the arrows
+	if(Input.is_action_pressed("ui_up")):
+		velocity -= 1200*delta
+		velocity = clamp(velocity, -maxSpeed, maxSpeed)
+	elif(Input.is_action_pressed("ui_down")):
+		velocity += 1200*delta
+		velocity = clamp(velocity, -maxSpeed, maxSpeed)
+	else:
+		if(velocity < 0):
+			if(velocity >= -42):
+				velocity = 0
+			else:
+				velocity += 42
+		elif(velocity > 0):
+			if(velocity <= 42):
+				velocity = 0
+			else:
+				velocity -= 42
+	$Camera2D.position.y += velocity*delta
+	if(len(images)%2 == 0):
+		$Camera2D.position.y = clamp($Camera2D.position.y, 0, max(len(images)*270-900, 0))
+	else:
+		$Camera2D.position.y = clamp($Camera2D.position.y, 0, max(len(images)*270-900+270, 0))
+	print(velocity)
